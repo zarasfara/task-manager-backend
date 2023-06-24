9f853684790f7ac1312a0be66ea86c23ce3e0372
zarasfara <85435988+zarasfara@users.noreply.github.com>
2023-06-24 12:42:03 +0500

    Remove unnecessary (#4)
    * refactor: Удалил ненужное
    Удалил ассеты, вьюшки, контроллеры
    
    * chore: обновил версию проекта до 10
    
    * chore: обновил тесты
    
    Обновил конфиги тестов и удалил устарешвие тесты

--- config/ziggy.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'except'=> ['api.v1.*']
-];
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/check', function () {
+    return "check success!";
+});
